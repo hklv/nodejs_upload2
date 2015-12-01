a7ec2fc745316d5ee9699eee8001732def476e06
hklv <1332280198@qq.com>
2015-12-01 20:01:16 +0800

    first commit

---
+++ server.js
@@ -0,0 +1,36 @@
+var http = require("http");
+var url = require("url");
+
+function start(route,handle) {
+  function onRequest(request, response) {
+	  var postData = "";
+	  var pathname = url.parse(request.url).pathname;
+	  if(pathname!=="/favicon.ico"){
+	  request.setEncoding("utf8");
+	  request.addListener("data",function(postDataChunk){
+		  postData +=postDataChunk;
+		  console.log("Received POST data chunk '"+
+      postDataChunk + "'.");
+	  });
+	  request.addListener("end",function(){
+		  route(handle,pathname,response,postData);
+	  });
+	/*
+   if(pathname!=="/favicon.ico"){
+    console.log("Request for "+pathname+" received.");
+	route(handle,pathname,response); 
+	/*
+    response.writeHead(200, {"Content-Type": "text/plain"});
+	var content = route(handle, pathname);
+    response.write(content);
+    response.end();
+	
+	}*/
+	  }
+  }
+
+  http.createServer(onRequest).listen(8822);
+  console.log("Server has started.");
+}
+
+exports.start = start;
